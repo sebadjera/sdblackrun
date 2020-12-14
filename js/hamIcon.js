7444018e5912ac78966d2e83edaca44fbff83781
Sebas! <72949410+sebadjera@users.noreply.github.com>
2020-12-14 11:48:56 -0300

    Add files via upload

---
+++ js/hamIcon.js
@@ -0,0 +1,5 @@
+function toggleClass(){
+    let menu = document.querySelector(".mainMenu");
+    menu.classList.toggle("toggleCls");
+    menu.classList.add('animate__animated', 'animate__fadeInDown');
+}
